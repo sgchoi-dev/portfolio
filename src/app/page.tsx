c33c2ae78304f4c9e1e1382c5e488b0e989cea9b
sgchoi-dev <choi.sg.dev@gmail.com>
2025-04-07 16:51:18 +0900

    main 페이지 작업 중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,90 +1,40 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+// import "../css/index.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>
-          첫화면 입니다~~~
-        </h1>
-        <ol>          
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <main>
+      <div className="main-my-info">
+        <div className="name-area">
+          <p className="name">
+            TEST
+            <br />
+            TEST
+            <br />
+            TEST
+          </p>
+          {/* <p className="name">
+            CHOI
+            <br />
+            SEUL
+            <br />
+            GI
+          </p> */}
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className="bot-area">
+          <div className="my-detail-info">
+            <a className="list tel" href="tel:01046926404">
+              010-4692-6404
+            </a>
+            <a className="list eamil" href="mailto:choi.sg.dev@gmail.com">
+              choi.sg.dev@gmail.com
+            </a>
+          </div>
+          <p className="copyright">©2025 sgChoi. All Rights Reserved.</p>
+        </div>
+      </div>
+    </main>
   );
 }
